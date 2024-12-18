a087f687ea06504c18b1c5b926309c23a98700f5
ozgealbayrakunal <ozge.albayrak10@gmail.com>
2024-12-18 21:17:15 +0300

    Add files via upload

---
+++ PediatricEndocrinologyForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace HospitalSystemProposal
+{
+    partial class PediatricEndocrinologyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 135);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "doktor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(602, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ANASAYFA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 20;
+            this.listBox4.Location = new System.Drawing.Point(811, 242);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(135, 124);
+            this.listBox4.TabIndex = 23;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(458, 242);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(318, 124);
+            this.listBox3.TabIndex = 22;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(42, 242);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(269, 124);
+            this.listBox2.TabIndex = 21;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(317, 242);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(135, 124);
+            this.listBox1.TabIndex = 20;
+            // 
+            // PediatricEndocrinologyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 418);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "PediatricEndocrinologyForm";
+            this.Text = "PediatricEndocrinologyForm";
+            this.Load += new System.EventHandler(this.PediatricEndocrinologyForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
